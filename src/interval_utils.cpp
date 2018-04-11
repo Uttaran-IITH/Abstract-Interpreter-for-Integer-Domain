acc86f500583bfe50e4ba60f4b1fa6046d79945b
Uttaran-IITH <37771739+Uttaran-IITH@users.noreply.github.com>
2018-04-11 16:51:25 +0530

    Add files via upload

--- src/interval_utils.cpp
+++ src/interval_utils.cpp
@@ -1,6 +1,13 @@
 #include "interval_utils.h"
 #include <math.h>
 #include <stdlib.h>
+#include <iostream>
+int min(int a,int b){
+	if (a<b)
+	return a;
+	else
+	return b;
+}
 void add(interval a, interval b, interval *c) {
 	c->set_lower_bound(a.get_lower_bound() + b.get_lower_bound());
 	c->set_upper_bound(a.get_upper_bound() + b.get_upper_bound());
@@ -50,43 +57,11 @@ void less_than(interval *a, interval *b) {
 		return;
 	}
 	else if (u2 < l1) {
-		a->set_lower_bound(NULL);
-		a->set_upper_bound(NULL);
-		return;
+		std::cout<<"Not valid";
 	}
 	else {
-		if (l1 < l2)
-			a->set_lower_bound(l2);
-		else
-			a->set_lower_bound(l1);
-		if (u1 < u2)
-			a->set_upper_bound(u1);
-		else
-			a->set_upper_bound(u2);
-	}
-}
-void less_than_equals(interval *a, interval *b) {
-	int l1 = a->get_lower_bound();
-	int u1 = a->get_upper_bound();
-	int l2 = b->get_lower_bound();
-	int u2 = b->get_upper_bound();
-	if (u1 < l2) {
-		return;
-	}
-	else if (u2 < l1) {
-		a->set_lower_bound(NULL);
-		a->set_upper_bound(NULL);
-		return;
-	}
-	else {
-		if (l1 < l2)
-			a->set_lower_bound(l2);
-		else
-			a->set_lower_bound(l1);
-		if (u1 < u2)
-			a->set_upper_bound(u1);
-		else
-			a->set_upper_bound(u2);
+		a->set_lower_bound(l1);
+		a->set_upper_bound(min(u1,u2));
 	}
 }
 void equals(interval *a, interval *b) {
@@ -95,18 +70,10 @@ void equals(interval *a, interval *b) {
 	int l2 = b->get_lower_bound();
 	int u2 = b->get_upper_bound();
 	if (u1 < l2 || u2 < l1) {
-		a->set_lower_bound(NULL);
-		a->set_upper_bound(NULL);
-		return;
+		std::cout<<"Invalid";
 	}
 	else {
-		if (l1 < l2)
-			a->set_lower_bound(l2);
-		else
-			a->set_lower_bound(l1);
-		if (u1 < u2)
-			a->set_upper_bound(u1);
-		else
-			a->set_upper_bound(u2);
+		a->set_lower_bound(max(l1,l2));
+		a->set_upper_bound(min(u1,u2));
 	}
 }
